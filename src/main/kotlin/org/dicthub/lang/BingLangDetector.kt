272ae3a45b29df5440abb4d41fadedf5b31a11b7
willings <willings.zhang@gmail.com>
2019-06-09 00:48:27 +0900

    Fix bing redirect problem

--- src/main/kotlin/org/dicthub/lang/BingLangDetector.kt
+++ src/main/kotlin/org/dicthub/lang/BingLangDetector.kt
@@ -1,83 +1,59 @@
 package org.dicthub.lang
 
-import browserObj
 import org.dicthub.util.HttpAsyncClient
+import org.w3c.dom.get
+import org.w3c.dom.set
+import org.w3c.dom.url.URL
+import org.w3c.xhr.XMLHttpRequest
+import kotlin.browser.localStorage
 import kotlin.js.Promise
-import kotlin.js.json
 
 class BingLangDetector(private val httpAsyncClient: HttpAsyncClient) : LangDetector {
 
     override fun detectLang(text: String): Promise<Lang> {
+
+        val detectLangWithNewContext = fun(resolve: (Lang) -> Unit, reject: (Throwable) -> Unit) {
+            getBingContext().then { newContext ->
+                saveContextToCache(newContext)
+                console.info("Detect lang use new context $newContext")
+                detectLangWithContext(newContext, text).then(resolve).catch(reject)
+            }
+        }
+
         return Promise { resolve, reject ->
-            detectLangUsingCachedToken(text).then(resolve).catch {
-                detectLangUsingNewToken(text).then(resolve).catch(reject)
+
+            loadCachedContext()?.let { cachedContext ->
+                console.info("Detect lang use cached context $cachedContext")
+                detectLangWithContext(cachedContext, text).then(resolve).catch {
+                    detectLangWithNewContext(resolve, reject)
+                }
+            } ?: run {
+                detectLangWithNewContext(resolve, reject)
             }
         }
     }
 
-    private fun detectLangUsingCachedToken(text: String): Promise<Lang> {
+    private fun detectLangWithContext(context: BingContext, text: String): Promise<Lang> {
         return Promise { resolve, reject ->
-            cachedToken().then { igg ->
-                callBingDetectApi(igg, text).then { code ->
-                    convertBingLangCodeToLang(code)
-                            ?.let(resolve)
-                            ?:reject(IllegalArgumentException("Failed to convert bing lang code $code"))
-                }.catch(reject)
+            callBingDetectApi(context, text).then { code ->
+                convertBingLangCodeToLang(code)
+                        ?.let(resolve)
+                        ?: reject(IllegalArgumentException("Failed to convert bing lang code $code"))
             }.catch(reject)
         }
     }
 
-    private fun detectLangUsingNewToken(text: String): Promise<Lang> {
-        return Promise { resolve, reject ->
-            newToken().then { igg ->
-                callBingDetectApi(igg, text).then { code ->
-                    convertBingLangCodeToLang(code)
-                            ?.let(resolve)
-                            ?:reject(IllegalArgumentException("Failed to convert bing lang code $code"))
-                }.catch(reject)
-            }.catch(reject)
-        }
-    }
-
-    private fun callBingDetectApi(igg: String, text: String): Promise<String> {
-        val url = "https://www.bing.com/tdetect?&IG=$igg&IID=translator.5037.1"
+    private fun callBingDetectApi(context: BingContext, text: String): Promise<String> {
+        val url = "https://${context.domain}/tdetect?&IG=${context.token}&IID=translator.5037.1"
         return httpAsyncClient.post(url, mapOf("Content-Type" to "application/x-www-form-urlencoded"),
                 "&text=${encodeURIComponent(text)}")
-    }
-
-    private val tokenStorageKey = "bing_token_igg";
-    private fun cachedToken(): Promise<String> = Promise { resolve, reject ->
-        browserObj.storage.local.get(tokenStorageKey) { data ->
-            data[tokenStorageKey]?.let { it as? String }
-                    ?.let { igg ->
-                        console.info("Loaded bing lang detection token $igg")
-                        resolve(igg)
-                    }
-                    ?: reject(IllegalStateException("No cached token"))
-        }
-    }
-
-    private val iggRegex = Regex("IG:\"([\\w\\d]+)\"")
-
-    private fun newToken(): Promise<String> = Promise { resolve, reject ->
-            httpAsyncClient.get("https://www.bing.com/translator").then { htmlContent ->
-                iggRegex.find(htmlContent)?.groupValues?.get(1)
-                        ?.let { igg ->
-                            browserObj.storage.local.set(json( tokenStorageKey to igg )) {
-                                console.info("Cached bing lang detection token $igg")
-                            }
-                            resolve(igg)
-                        }
-                        ?: run {
-                            reject(IllegalStateException("No IG found in html content"))
-                        }
-            }.catch(reject)
     }
 
     private val knownBingLangMap = mapOf(
             "zh-CHS" to Lang.ZH_CN,
             "zh-CHT" to Lang.ZH_TW
     )
+
     private fun convertBingLangCodeToLang(code: String): Lang? {
         // Most cases direct match
         return fromCode(code)
@@ -85,6 +61,47 @@ class BingLangDetector(private val httpAsyncClient: HttpAsyncClient) : LangDetec
                 ?: knownBingLangMap[code]
     }
 
+    private val domainStorageKey = "extension-bing-domain"
+    private val tokenStorageKey = "extension-bing-token"
+
+    private fun loadCachedContext(): BingContext? {
+        val domain = localStorage[domainStorageKey] ?: return null
+        val token = localStorage[tokenStorageKey] ?: return null
+        return BingContext(domain = domain, token = token)
+    }
+
+    private fun saveContextToCache(context: BingContext) {
+        localStorage[domainStorageKey] = context.domain
+        localStorage[tokenStorageKey] = context.token
+    }
+
+    private fun getBingContext(): Promise<BingContext> {
+        return Promise { resolve, reject ->
+            val xhr = XMLHttpRequest()
+            xhr.addEventListener("load", { _ ->
+                val url = URL(xhr.responseURL)
+                val htmlContent = xhr.responseText
+                iggRegex.find(htmlContent)?.groupValues?.get(1)?.let { token ->
+                    resolve(BingContext(domain = url.hostname, token = token))
+                } ?: run {
+                    reject(IllegalStateException("No IG found in html content"))
+                }
+            })
+            xhr.addEventListener("error", { event ->
+                reject(IllegalStateException("Failed to fetch url, error event: $event"))
+            })
+            xhr.open("GET", DEFAULT_URL)
+            xhr.send()
+        }
+    }
+
+    private val DEFAULT_URL = "https://www.bing.com/translator"
+    private val iggRegex = Regex("IG:\"([\\w\\d]+)\"")
+
+    data class BingContext(
+            val domain: String,
+            val token: String
+    )
 }
 
 private external fun encodeURIComponent(text: String): String
